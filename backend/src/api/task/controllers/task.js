4962fd1850715fd62123ca583a78c81cadc42bf2
YAP! <gabriele.maddaloni@gmail.com>
2024-11-09 17:44:36 +0000

    created controller override to get tasks.

--- backend/src/api/task/controllers/task.js
+++ backend/src/api/task/controllers/task.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * task controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::task.task');
+module.exports = createCoreController("api::task.task", ({ strapi }) => ({
+  async find(ctx) {
+    const { user } = ctx.state;
+
+    if (!user) {
+      return ctx.unauthorized("You cant access those tasks my dude");
+    }
+
+    const tasks = await strapi.documents("api::task.task").findMany({
+      filters: {
+        owner: user.id,
+      },
+    });
+
+    return ctx.send(tasks);
+  },
+}));
